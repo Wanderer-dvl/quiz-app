09767aa77e9ed560b730d70ee3633c76d52146a7
Wanderer <102596701+Wanderer-dvl@users.noreply.github.com>
2026-08-31 10:35:54 +0700

    update quiz.js state

---
+++ src/components/Quiz.js
@@ -0,0 +1,20 @@
+import QuestionsData from '../data/QuestionsData';
+import { useState } from 'react';
+const Quiz = () => {
+    //console.log(QuestionsData)
+    const [current, setCurrent] = useState(0)
+    const [selectedChoice, setSelectedChoice] = useState("")
+    return(
+        <div className="quiz">
+            <h1>{QuestionsData[current].question}</h1>
+            <div className="choices">
+                <button onClick={()=>setSelectedChoice("A")}>{QuestionsData[current].A}</button>
+                <button onClick={()=>setSelectedChoice("B")}>{QuestionsData[current].B}</button>
+                <button onClick={()=>setSelectedChoice("C")}>{QuestionsData[current].C}</button>
+                <button onClick={()=>setSelectedChoice("D")}>{QuestionsData[current].D}</button>
+            </div>
+            <p>{`${current + 1}/${QuestionsData.length}`}</p>
+        </div>
+    )
+}
+export default Quiz;
