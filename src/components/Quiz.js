3b75cca05685ea44151548eeabda8a1254c1b42d
Wanderer <102596701+Wanderer-dvl@users.noreply.github.com>
2026-08-31 11:13:28 +0700

    update quiz-app 1-36

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,17 +1,43 @@
+import { useContext,useState,useEffect } from 'react';
+import { DataContext } from '../App';
 import QuestionsData from '../data/QuestionsData';
-import { useState } from 'react';
 const Quiz = () => {
     //console.log(QuestionsData)
     const [current, setCurrent] = useState(0)
-    const [selected, setSelected] = useState("")
-    return(
+    const [selectChoice, setSelectChoice] = useState("")
+    const {score, setScore,setAppState} = useContext(DataContext)
+
+    useEffect(() => {
+     checkAnswer()
+    }, [selectChoice])
+    const checkAnswer = () => {
+        if(selectChoice !== ""){
+            if(selectChoice === QuestionsData[current].answer){    
+                setScore(score + 1)
+                nextQuestion()
+            }else{
+                nextQuestion()
+            }
+    } 
+}
+    const nextQuestion = () => {
+        setSelectChoice("")
+        if(current === QuestionsData.length - 1){
+            setAppState("score")
+        }else{
+            setCurrent(current + 1)
+        }
+    }
+    
+
+ return(
         <div className="quiz">
             <h1>{QuestionsData[current].question}</h1>
             <div className="choices">
-                <button>{QuestionsData[current].A}</button>
-                <button>{QuestionsData[current].B}</button>
-                <button>{QuestionsData[current].C}</button>
-                <button>{QuestionsData[current].D}</button>
+                <button onClick={()=>setSelectChoice("A")}>{QuestionsData[current].A}</button>
+                <button onClick={()=>setSelectChoice("B")}>{QuestionsData[current].B}</button>
+                <button onClick={()=>setSelectChoice("C")}>{QuestionsData[current].C}</button>
+                <button onClick={()=>setSelectChoice("D")}>{QuestionsData[current].D}</button>
             </div>
             <p>{`${current + 1}/${QuestionsData.length}`}</p>
         </div>
